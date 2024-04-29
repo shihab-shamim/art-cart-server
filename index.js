2817f0560f80353a2119e182d0db118e726c29a6
shihabosm <shihabshamim767@gmail.com>
2024-04-29 13:14:04 +0600

    orgin set

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://assaiment-10-auth.web.app'],
+};
 
 // middleware  
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u53e1so.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
